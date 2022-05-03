24e17e8812f861142ef461a2ff961dfb6f35a494
furev.mykola <aleniki21@gmail.com>
2022-05-03 20:10:14 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 
 function createMarkupGalleryImg(gallery) {
   const galleryImg = gallery
     .map(({ preview, original, description }) => {
-      return `
+      return `<li>
     <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a> `;
+</a></li> `;
     })
-    .join('');
-  galleryEl.insertAdjacentHTML('beforeend', galleryImg);
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", galleryImg);
 }
 createMarkupGalleryImg(galleryItems);
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
   captionDelay: 250,
 });
 // const lightbox = $('.gallery a').simpleLightbox({
